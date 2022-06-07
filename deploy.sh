cd1d4a1076759d2dace257c1b918a2b95483db6b
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-06-07 17:52:12 +0200

    feat: deployment script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+set -e
+set -x
+
+source .env.deployment
+
+echo "Syncing root files"
+  src_dir="${DIR}/public/"
+  dst_dir="${BASE_DIR}"
+  rsync -raP --cvs-exclude \
+    -e "ssh -o StrictHostKeyChecking=no -i ${SSH_PRIVKEY_PATH} -p ${SSH_PORT}" \
+    "${src_dir}" \
+    "${SSH_USER}@${SSH_HOST}:${dst_dir}"
+  echo "-- done --"
+  echo ""
