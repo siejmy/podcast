7aeb05ac124ffccb1a7a13b6756657262f2ba125
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-06-07 17:57:01 +0200

    ci: fixed ssh key path

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,16 @@ set -e
 set -x
 
 source .env
+SSH_PRIVKEY_PATH=.ssh.privkey
+SRC_DIR="${DIR}/public/"
+DST_DIR="${BASE_DIR}"
 
 echo "Syncing root files"
-  src_dir="${DIR}/public/"
-  dst_dir="${BASE_DIR}"
+  src_dir="${SRC_DIR}"
+  dst_dir="${DST_DIR}"
   rsync -raP --cvs-exclude \
     -e "ssh -o StrictHostKeyChecking=no -i ${SSH_PRIVKEY_PATH} -p ${SSH_PORT}" \
     "${src_dir}" \
     "${SSH_USER}@${SSH_HOST}:${dst_dir}"
-  echo "-- done --"
-  echo ""
+echo "-- done --"
+echo ""
